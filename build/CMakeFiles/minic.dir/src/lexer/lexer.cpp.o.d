3d7bfc66793a720da549992408aa557399997931
Dinkar Upadhyay <dinkarupadhyay2906@gmail.com>
2026-06-13 14:10:22 -0400

    Add lexer skeleton

---
+++ build/CMakeFiles/minic.dir/src/lexer/lexer.cpp.o.d
@@ -0,0 +1,5 @@
+CMakeFiles/minic.dir/src/lexer/lexer.cpp.o: \
+ /home/dinkar/Desktop/mini-c-compiler/src/lexer/lexer.cpp \
+ /usr/include/stdc-predef.h \
+ /home/dinkar/Desktop/mini-c-compiler/include/lexer.h \
+ /home/dinkar/Desktop/mini-c-compiler/include/token.h
---
+++ build/CMakeFiles/minic.dir/src/main.cpp.o.d
@@ -0,0 +1,5 @@
+CMakeFiles/minic.dir/src/main.cpp.o: \
+ /home/dinkar/Desktop/mini-c-compiler/src/main.cpp \
+ /usr/include/stdc-predef.h \
+ /home/dinkar/Desktop/mini-c-compiler/include/lexer.h \
+ /home/dinkar/Desktop/mini-c-compiler/include/token.h
