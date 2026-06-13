cadf0335463104c6cad4944c33d6e0c2dfd671bd
Dinkar Upadhyay <dinkarupadhyay2906@gmail.com>
2026-06-13 14:18:15 -0400

    Add number tokenization

--- build/CMakeFiles/minic.dir/src/lexer/lexer.cpp.o.d
+++ build/CMakeFiles/minic.dir/src/lexer/lexer.cpp.o.d
@@ -2,4 +2,23 @@ CMakeFiles/minic.dir/src/lexer/lexer.cpp.o: \
  /home/dinkar/Desktop/mini-c-compiler/src/lexer/lexer.cpp \
  /usr/include/stdc-predef.h \
  /home/dinkar/Desktop/mini-c-compiler/include/lexer.h \
- /home/dinkar/Desktop/mini-c-compiler/include/token.h
+ /home/dinkar/Desktop/mini-c-compiler/include/token.h \
+ /usr/include/c++/12/cctype \
+ /usr/include/x86_64-linux-gnu/c++/12/bits/c++config.h \
+ /usr/include/x86_64-linux-gnu/c++/12/bits/os_defines.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/bits/timesize.h \
+ /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/long-double.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/include/x86_64-linux-gnu/c++/12/bits/cpu_defines.h \
+ /usr/include/c++/12/pstl/pstl_config.h /usr/include/ctype.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+ /usr/include/x86_64-linux-gnu/bits/time64.h \
+ /usr/include/x86_64-linux-gnu/bits/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/endianness.h \
+ /usr/include/x86_64-linux-gnu/bits/types/locale_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__locale_t.h
