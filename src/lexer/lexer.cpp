ee780254d23019ce5011e9e52219bcfa3978d835
Dinkar Upadhyay <dinkarupadhyay2906@gmail.com>
2026-06-30 04:41:45 +0530

    lexer initial stage

--- src/lexer/lexer.cpp
+++ src/lexer/lexer.cpp
@@ -1,21 +1,83 @@
-#ifndef LEXER_H
-#define LEXER_H
+#include "lexer.h"
 
-#include "token.h"
+#include <cctype>
 
-class Lexer
+Lexer::Lexer(const char* src)
 {
-private:
-    const char* source;
-    int pos;
+    source = src;
+    pos = 0;
+}
 
-    char currentChar();
-    void advance();
+char Lexer::currentChar()
+{
+    return source[pos];
+}
 
-public:
-    Lexer(const char* src);
+void Lexer::advance()
+{
+    pos++;
+}
 
-    Token nextToken();
-};
+Token Lexer::nextToken()
+{
+    static char buffer[256];
 
-#endif
+    while (currentChar() != '\0')
+    {
+        // Skip spaces
+        if (isspace(currentChar()))
+        {
+            advance();
+            continue;
+        }
+
+        // Number
+        if (isdigit(currentChar()))
+        {
+            int i = 0;
+
+            while (isdigit(currentChar()))
+            {
+                buffer[i++] = currentChar();
+                advance();
+            }
+
+            buffer[i] = '\0';
+
+            Token token;
+            token.type = TOK_NUMBER;
+            token.lexeme = buffer;
+
+            return token;
+        }
+
+        // Identifier
+        if (isalpha(currentChar()) || currentChar() == '_')
+        {
+            int i = 0;
+
+            while (isalnum(currentChar()) || currentChar() == '_')
+            {
+                buffer[i++] = currentChar();
+                advance();
+            }
+
+            buffer[i] = '\0';
+
+            Token token;
+            token.type = TOK_IDENTIFIER;
+            token.lexeme = buffer;
+
+            return token;
+        }
+
+        // Ignore unknown characters
+        advance();
+    }
+
+    Token token;
+    token.type = TOK_EOF;
+    token.lexeme = "";
+
+    return token;
+}
