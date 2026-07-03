485da4e30563d421aef1aae934d18c34707e3bff
Dinkar Upadhyay <dinkarupadhyay2906@gmail.com>
2026-07-03 09:55:02 +0530

    build hatao

--- build/CMakeFiles/minic.dir/src/main.cpp.o.d
+++
@@ -1,6 +0,0 @@
-CMakeFiles/minic.dir/src/main.cpp.o: \
- /home/dinkar/Desktop/mini-c-compiler/src/main.cpp \
- /usr/include/stdc-predef.h \
- /home/dinkar/Desktop/mini-c-compiler/include/parser.h \
- /home/dinkar/Desktop/mini-c-compiler/include/lexer.h \
- /home/dinkar/Desktop/mini-c-compiler/include/token.h
